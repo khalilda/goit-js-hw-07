3cd875091e957fce2a5acd418fc9cbb6019e2e21
khalilda <100872154+khalilda@users.noreply.github.com>
2023-05-28 17:27:50 +0300

    homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.01-gallery');
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = '#';
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = item.smallImage;
+  image.alt = item.description;
+  image.setAttribute('data-source', item.largeImage);
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+
+  return galleryItem;
+}
+
+galleryItems.forEach(item => {
+  const galleryItem = createGalleryItem(item);
+  gallery.appendChild(galleryItem);
+});
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+
+  const target = event.target;
+  if (target.nodeName !== 'IMG') return;
+
+  const largeImageURL = target.dataset.source;
+
+  const lightbox = basicLightbox.create(`
+    <img src="${largeImageURL}" alt="Full Size Image" />
+  `);
+
+  lightbox.element().querySelector('img').src = largeImageURL;
+
+  lightbox.show();
+
+  const closeLightbox = () => lightbox.close();
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      closeLightbox();
+    }
+  });
+});
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const lightbox = basicLightbox.create(`
+<img src="${largeImageURL}" alt="Full Size Image" />
+`);
 
+
+lightbox.element().querySelector('img').src = largeImageURL;
+
+lightbox.show();
+
+
+const closeLightbox = () => lightbox.close();
+document.addEventListener('keydown', event => {
+if (event.key === 'Escape') {
+  closeLightbox();
+};
+});
 console.log(galleryItems);
